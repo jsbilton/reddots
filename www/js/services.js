10429b5ebc2f8f4e52a8f2f9152a457ab5a2c9e4
Jared Bilton <cantops@Jareds-MacBook-Pro.local>
2015-12-11 14:40:47 -0500

    Replaced some files

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,25 @@
+// Services provide data
+
 (function() {
   'use strict';
+  
   angular
-  .factory('function', )
+  .module('starter')
+  .factory('ReddotService', function ($http) { //Google API
+    var urlOpts = {
+        baseUrl: "http://tiny-tiny.herokuapp.com/collections/reddots";
+        apiKey: "",
+        method: "",
+        format: "json",
+        buildUrl: function () {
+          // body...
+        }
+
+      }
+  } )
+
+
+
+
+
 }());
