b5076c12619774aee25ab5e35726691526c4ab39
Jared Bilton <cantops@Jareds-MacBook-Pro.local>
2015-12-14 14:04:10 -0500

    worked on store view inputs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
 
 angular.module('reddots.controllers', [
+  'ionic',
   'ngMap',
   'ngCordova'
 ])
@@ -28,83 +29,3 @@ angular.module('reddots.controllers', [
 .controller('OwnerCtrl', function($scope, $stateParams) {
   $scope.createowner = owner;
 });
-
-
-
-
-// .controller('StoreviewCtrl', function($scope, $cordovaBarcodeScanner) {
-//
-//     $scope.scanBarcode = function() {
-//         $cordovaBarcodeScanner.scan().then(function(imageData) {
-//             alert(imageData.text);
-//             console.log("Barcode Format -> " + imageData.format);
-//             console.log("Cancelled -> " + imageData.cancelled);
-//         }, function(error) {
-//             console.log("An error happened -> " + error);
-//         });
-//     };
-
-// .controller('StoreviewCtrl', function($scope) {
-//     $scope.storeview = storeview;
-//
-// })
-//
-// .controller('StoreviewCtrl', function($scope, $stateParams) {
-//
-// });
-
-
-
-
-
-
-
-// .controller('PopupCtrl',function($scope, $ionicPopup, $timeout) {
-//
-// // Triggered on a button click, or some other target
-// $scope.showPopup = function() {
-//   $scope.data = {}
-//
-//   // An elaborate, custom popup
-//   var myPopup = $ionicPopup.show({
-//     template: '',
-//     title: 'Confirm Account ',
-//     subTitle: 'Please use normal things',
-//     scope: $scope,
-//     buttons: [
-//       { text: 'Cancel' },
-//       {
-//         text: '<b>Save</b>',
-//         type: 'button-positive',
-//         onTap: function(e) {
-//           if (!$scope.data.wifi) {
-//             //don't allow the user to close unless he enters wifi password
-//             e.preventDefault();
-//           } else {
-//             return $scope.data.wifi;
-//           }
-//         }
-//       }
-//     ]
-//   });
-//   myPopup.then(function(res) {
-//     console.log('Tapped!', res);
-//   });
-//   $timeout(function() {
-//      myPopup.close(); //close the popup after 3 seconds for some reason
-//   }, 3000);
-//  };
-//  // A confirm dialog
-//  $scope.showConfirm = function() {
-//    var confirmPopup = $ionicPopup.confirm({
-//      title: 'Confirm Account',
-//      template: 'Are you sure you want to confirm account?'
-//    });
-//    confirmPopup.then(function(res) {
-//      if(res) {
-//        console.log('You are sure');
-//      } else {
-//        console.log('You are not sure');
-//      }
-//    });
-//  };
--- www/js/services.js
+++ www/js/services.js
@@ -1,21 +1,21 @@
 // Services provide data
 
-(function() {
-  'use strict';
-
-  angular
-  .module('reddots.services')
-  .factory('ReddotService', function ($http) { //Google API
-    var urlOpts = {
-        apiKey: "AIzaSyDhRWpt5iVgJj2MXR2YPJnZNKzXbhEaGZk",
-        method: "",
-        format: "json",
-
-      };
-  });
-
-
-
-
-
-}());
+// (function() {
+//   'use strict';
+//
+//   angular
+//   .module('reddots.services')
+//   .factory('ReddotService', function ($http) { //Google API
+//     var urlOpts = {
+//         apiKey: "AIzaSyDhRWpt5iVgJj2MXR2YPJnZNKzXbhEaGZk",
+//         method: "",
+//         format: "json",
+//
+//       };
+//   });
+//
+//
+//
+//
+//
+// }());
