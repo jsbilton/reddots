d70e16064d74c4c40dbc080ca857e665d46865b8
Jet Balagtas <jetbalagtas@gmail.com>
2015-12-21 15:52:27 -0500

    mapbox integration

--- www/map/map.controllers.js
+++ www/map/map.controllers.js
@@ -5,81 +5,22 @@
 
 angular
 .module('map')
+.controller('MapCtrl', function($scope, MapService, mapboxService, $ionicLoading, $window) {
+  mapboxService.init({ accessToken: 'pk.eyJ1IjoiY2Fsd2ViIiwiYSI6IkQxUTJBS2cifQ.aS0kLu4cddiieYU2XoFGvg' });
+    $scope.map = {
+      center: {
+        latitude: 32.788969,
+        longitude: -79.938103
+      },
+      zoom: 8
+    };
+     MapService.getSpots().success(function (spots) {
+       $scope.spots = spots;
 
-.controller('MapCtrl', function($scope, $ionicLoading, $window) {
 
-  $scope.positions = [{
-    lat: 32.788990,
-    lng: -79.938120
-  }];
 
-    google.maps.event.addDomListener(window, 'load', function() {
-        var myLatlng = new google.maps.LatLng(32.788990, -79.938120);
-
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                title: "My Location"
-            });
-        });
-
-        $scope.map = map;
-    });
-    //   var liquor = new google.maps.Marker({
-    //    position: liquor,
-    //    map: map,
-    //    title: 'ABC Store'
-    //  });
+     });
+     $scope.addSpot = function (spot) {
+       MapService.createSpot(spot);
+     };
 });
-
-// var map;
-// var infowindow;
-//
-// function initMap() {
-//   var theIronYard = {lat: 32.7794, lng: -79.9341};
-//
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: theIronYard,
-//     zoom: 15
-//   });
-//
-//   infowindow = new google.maps.InfoWindow();
-//
-//   var service = new google.maps.places.PlacesService(map);
-//   service.nearbySearch({
-//     location: theIronYard,
-//     radius: 500,
-//     types: ['store']
-//   }, callback);
-// }
-//
-// function callback(results, status) {
-//   if (status === google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       createMarker(results[i]);
-//     }
-//   }
-// }
-//
-// function createMarker(place) {
-//   var placeLoc = place.geometry.location;
-//   var marker = new google.maps.Marker({
-//     map: map,
-//     position: place.geometry.location
-//   });
-//
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map, this);
-//   });
-// }
