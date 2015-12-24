c11f275b1002f7072c9986a0af5c89c3a1b23edf
Jet Balagtas <jetbalagtas@gmail.com>
2015-12-23 20:05:44 -0500

    added storeveiew list input, still needs to save data

--- www/stores/stores.services.js
+++ www/stores/stores.services.js
@@ -28,6 +28,32 @@ angular
 
   });
 
+angular
+  .module('stores')
+  .factory('ListFactory', function() {
+
+    var list = [];
+    var listStore = localStorage.getItem("list");
+    if (listStore !== null && listStore !== '' && angular.isArray(angular.fromJson(listStore))) {
+      list = angular.fromJson(listStore);
+    }
+    var listSrv = {
+      setList: function(newList) {
+        list = newList;
+        localStorage.setItem("list", angular.toJson(list));
+        return true;
+      },
+      getList: function() {
+        if (list !== null) {
+          return list;
+        } else {
+          return [];
+        }
+      }
+    };
+    return listSrv;
+  });
+
   // .factory('StoreViewService', function ($http) {
   //
   //     var addItem = function(newItem){
