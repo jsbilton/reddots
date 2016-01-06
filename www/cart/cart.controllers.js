9ab7082d259a8d33450b593d9268dc2fa2fd11e5
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2016-01-05 21:25:15 -0500

    quit trying to add the popup

--- www/cart/cart.controllers.js
+++ www/cart/cart.controllers.js
@@ -46,44 +46,45 @@ angular
       $scope.totalPriceValue = totalPrice;
     };
 
-    $scope.showConfirm = function() {
-       var confirmPopup = $ionicPopup.confirm({
-         title: 'All customers must show proper identification upon pickup',
-         template: 'Are you sure you are of the legal age of consumption?'
-       });
-       confirmPopup.then(function(res) {
-         if(res) {
-           console.log('You are sure');
-         } else {
-           console.log('You are not sure');
-         }
-       });
-     };
+        $scope.showConfirm = function() {
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Consume Ice Cream',
+                template: 'Are you sure you want to eat this ice cream?'
+            });
+            confirmPopup.then(function(res) {
+                if (res) {
+                    console.log('You are sure');
+                } else {
+                    console.log('You are not sure');
+                }
+            });
+        };
 
-    //Credit card payment
-    $scope.card = {
-    name: 'Mike Brown',
-    number: '5555 4444 3333 1111',
-    expiry: '11 / 2020',
-    cvc: '123'
-    };
+  });
+    // Credit card payment
+    // $scope.card = {
+    // name: 'Jon Snow',
+    // number: '5555 4444 3333 1111',
+    // expiry: '11 / 2020',
+    // cvc: '123'
+    // };
 
-    $scope.cardPlaceholders = {
-      name: 'Your Full Name',
-      number: 'xxxx xxxx xxxx xxxx',
-      expiry: 'MM/YY',
-      cvc: 'xxx'
-    };
-
-    $scope.cardMessages = {
-      validDate: 'valid\nthru',
-      monthYear: 'MM/YYYY',
-    };
-
-    $scope.cardOptions = {
-      debug: false,
-      formatting: true
-    };
+    // $scope.cardPlaceholders = {
+    //   name: 'Your Full Name',
+    //   number: 'xxxx xxxx xxxx xxxx',
+    //   expiry: 'MM/YY',
+    //   cvc: 'xxx'
+    // };
+    //
+    // $scope.cardMessages = {
+    //   validDate: 'valid\nthru',
+    //   monthYear: 'MM/YYYY',
+    // };
+    //
+    // $scope.cardOptions = {
+    //   debug: false,
+    //   formatting: true
+    // };
 
     // $scope.showConfirm = function() {
     //    var confirmPopup = $ionicPopup.confirm({
@@ -98,12 +99,3 @@ angular
     //      }
     //    });
     //  };
-
-
-
-
-
-
-
-
-    });
